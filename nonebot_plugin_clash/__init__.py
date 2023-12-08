f9fb0ad4169efa38a4f28b0d72d7dad1259d7ea5
student_2333 <lgc2333@126.com>
2023-12-08 15:43:05 +0800

    init

--- nonebot_plugin_clash/__init__.py
+++ nonebot_plugin_clash/__init__.py
@@ -7,11 +7,11 @@ from .config import ConfigModel  # noqa: E402
 
 __version__ = "0.1.0"
 __plugin_meta__ = PluginMetadata(
-    name="nonebot-plugin-example",
-    description="插件模板",
-    usage="这是一个一个一个插件模板",
+    name="Clash",
+    description="在 NoneBot 中控制你的 Clash",
+    usage="待补充",
     type="application",
-    homepage="https://github.com/owner/nonebot-plugin-example",
+    homepage="https://github.com/lgc-NB2Dev/nonebot-plugin-clash",
     config=ConfigModel,
     supported_adapters=inherit_supported_adapters("nonebot_plugin_alconna"),
     extra={"License": "MIT", "Author": "student_2333"},
